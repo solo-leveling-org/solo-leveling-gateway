03dfdf19239e6a2599ca57b5ec319f471cd37b20
sleepkqq <nikita32ivanov@gmail.com>
2025-02-26 00:06:36 +0300

    @Push annotation

--- src/main/java/com/sleepkqq/sololeveling/Application.java
+++ src/main/java/com/sleepkqq/sololeveling/Application.java
@@ -1,11 +1,15 @@
 package com.sleepkqq.sololeveling;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Push
+@EnableScheduling
 @SpringBootApplication
 @Theme(value = "my-app", variant = Lumo.DARK)
 public class Application implements AppShellConfigurator {
