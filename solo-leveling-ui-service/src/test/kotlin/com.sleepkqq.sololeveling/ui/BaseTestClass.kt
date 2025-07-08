7b64971d631bd24f26c7422f87ddfe5d2cd70071
sleepkqq <nikita32ivanov@gmail.com>
2025-07-08 13:47:29 +0300

    Enable test profile for integration tests.

--- solo-leveling-ui-service/src/test/kotlin/com.sleepkqq.sololeveling/ui/BaseTestClass.kt
+++ solo-leveling-ui-service/src/test/kotlin/com.sleepkqq.sololeveling/ui/BaseTestClass.kt
@@ -1,11 +1,13 @@
 package com.sleepkqq.sololeveling.ui
 
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.kafka.ConfluentKafkaContainer
 
 @Testcontainers
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 abstract class BaseTestClass {
 
