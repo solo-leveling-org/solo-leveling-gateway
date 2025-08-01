0ff0510c7acaf1b04fe89c399f1c302d128a0b14
Nikita <107306032+sleepkqq@users.noreply.github.com>
2025-08-01 14:59:52 +0300

    Removed retryable annotations from gRPC clients.

--- solo-leveling-gateway-service/src/main/kotlin/com.sleepkqq.sololeveling/gateway/grpc/client/PlayerGrpcApi.kt
+++ solo-leveling-gateway-service/src/main/kotlin/com.sleepkqq.sololeveling/gateway/grpc/client/PlayerGrpcApi.kt
@@ -2,7 +2,6 @@ package com.sleepkqq.sololeveling.gateway.grpc.client
 
 import com.sleepkqq.sololeveling.proto.player.*
 import com.sleepkqq.sololeveling.proto.player.PlayerServiceGrpc.PlayerServiceBlockingStub
-import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,32 +9,26 @@ class PlayerGrpcApi(
 	private val playerStub: PlayerServiceBlockingStub
 ) {
 
-	@Retryable
 	fun getActiveTasks(playerId: Long): GetActiveTasksResponse = playerStub.getActiveTasks(
 		GetActiveTasksRequest.newBuilder().setPlayerId(playerId).build()
 	)
 
-	@Retryable
 	fun getPlayerTopics(playerId: Long): GetPlayerTopicsResponse = playerStub.getPlayerTopics(
 		GetPlayerTopicsRequest.newBuilder().setPlayerId(playerId).build()
 	)
 
-	@Retryable
 	fun savePlayerTopics(request: SavePlayerTopicsRequest) {
 		playerStub.savePlayerTopics(request)
 	}
 
-	@Retryable
 	fun generateTasks(playerId: Long) {
 		playerStub.generateTasks(GenerateTasksRequest.newBuilder().setPlayerId(playerId).build())
 	}
 
-	@Retryable
 	fun completeTask(request: CompleteTaskRequest) {
 		playerStub.completeTask(request)
 	}
 
-	@Retryable
 	fun skipTask(request: SkipTaskRequest) {
 		playerStub.skipTask(request)
 	}
--- solo-leveling-gateway-service/src/main/kotlin/com.sleepkqq.sololeveling/gateway/grpc/client/UserGrpcApi.kt
+++ solo-leveling-gateway-service/src/main/kotlin/com.sleepkqq.sololeveling/gateway/grpc/client/UserGrpcApi.kt
@@ -5,7 +5,6 @@ import com.sleepkqq.sololeveling.proto.user.GetUserRequest
 import com.sleepkqq.sololeveling.proto.user.UserInput
 import com.sleepkqq.sololeveling.proto.user.UserServiceGrpc.UserServiceBlockingStub
 import com.sleepkqq.sololeveling.proto.user.UserView
-import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Service
 
 @Service
@@ -13,13 +12,11 @@ class UserGrpcApi(
 	private val userStub: UserServiceBlockingStub
 ) {
 
-	@Retryable
 	fun getUser(userId: Long): UserView = userStub.getUser(
 		GetUserRequest.newBuilder().setUserId(userId).build()
 	)
 		.user
 
-	@Retryable
 	fun authUser(userInput: UserInput) {
 		userStub.authUser(AuthUserRequest.newBuilder().setUser(userInput).build())
 	}
