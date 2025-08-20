3397c22e7b899153b5fa834c6b288cc8e3476267
Nikita <107306032+sleepkqq@users.noreply.github.com>
2025-08-20 18:49:04 +0300

    Fix companion object usage for Application class and updates Spring Boot parent version to 3.5.4.

--- solo-leveling-gateway-service/src/main/kotlin/com.sleepkqq.sololeveling/gateway/Application.kt
+++ solo-leveling-gateway-service/src/main/kotlin/com.sleepkqq.sololeveling/gateway/Application.kt
@@ -1,18 +1,14 @@
 package com.sleepkqq.sololeveling.gateway
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.retry.annotation.EnableRetry
 
 @EnableRetry
 @SpringBootApplication
-class Application : SpringBootServletInitializer() {
+class Application : SpringBootServletInitializer()
 
-	companion object {
-		@JvmStatic
-		fun main(args: Array<String>) {
-			SpringApplication.run(Application::class.java, *args)
-		}
-	}
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
 }
