0078dfdbd25d38fa49393606d084a82bbaaaa449
Nikita <107306032+sleepkqq@users.noreply.github.com>
2025-08-20 19:29:33 +0300

    Update jjwt version and configure native image reflection.

--- solo-leveling-gateway-service/src/main/kotlin/com.sleepkqq.sololeveling/gateway/Application.kt
+++ solo-leveling-gateway-service/src/main/kotlin/com.sleepkqq.sololeveling/gateway/Application.kt
@@ -3,8 +3,18 @@ package com.sleepkqq.sololeveling.gateway
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.springframework.nativex.hint.TypeAccess
+import org.springframework.nativex.hint.TypeHint
 import org.springframework.retry.annotation.EnableRetry
 
+@TypeHint(
+	typeNames = [
+		"io.jsonwebtoken.impl.security.KeysBridge",
+		"io.jsonwebtoken.impl.DefaultJwtParser",
+		"io.jsonwebtoken.impl.DefaultJwtBuilder"
+	],
+	access = [TypeAccess.DECLARED_METHODS, TypeAccess.DECLARED_FIELDS]
+)
 @EnableRetry
 @SpringBootApplication
 class Application : SpringBootServletInitializer()
