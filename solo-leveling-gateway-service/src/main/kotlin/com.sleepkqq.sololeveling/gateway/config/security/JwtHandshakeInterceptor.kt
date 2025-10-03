0134d91888153e44ce4889a62e27193ddf68492b
Nikita <107306032+sleepkqq@users.noreply.github.com>
2025-10-03 18:15:36 +0300

    Add SecurityContextHolder to JwtHandshakeInterceptor and fix NotificationService file ending

--- solo-leveling-gateway-service/src/main/kotlin/com.sleepkqq.sololeveling/gateway/config/security/JwtHandshakeInterceptor.kt
+++ solo-leveling-gateway-service/src/main/kotlin/com.sleepkqq.sololeveling/gateway/config/security/JwtHandshakeInterceptor.kt
@@ -8,6 +8,7 @@ import org.springframework.http.server.ServerHttpRequest
 import org.springframework.http.server.ServerHttpResponse
 import org.springframework.http.server.ServletServerHttpRequest
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.context.SecurityContextImpl
 import org.springframework.stereotype.Component
 import org.springframework.web.socket.WebSocketHandler
@@ -52,6 +53,7 @@ class JwtHandshakeInterceptor(
 			val authentication = UsernamePasswordAuthenticationToken(user, jwt, user.authorities)
 			val securityContext = SecurityContextImpl(authentication)
 			attributes[SPRING_SECURITY_CONTEXT] = securityContext
+			SecurityContextHolder.getContext().authentication = authentication
 
 			true
 
