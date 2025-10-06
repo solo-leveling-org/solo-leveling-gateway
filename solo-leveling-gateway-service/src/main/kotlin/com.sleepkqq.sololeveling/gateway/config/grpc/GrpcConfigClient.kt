2fb2eb7698cf56e15b65d6cabd1088b59faa8374
Nikita <107306032+sleepkqq@users.noreply.github.com>
2025-10-07 01:42:30 +0300

    Remove LocaleClientInterceptor and update solo-leveling-proto version to 4.5.1

--- solo-leveling-gateway-service/src/main/kotlin/com.sleepkqq.sololeveling/gateway/config/grpc/GrpcConfigClient.kt
+++ solo-leveling-gateway-service/src/main/kotlin/com.sleepkqq.sololeveling/gateway/config/grpc/GrpcConfigClient.kt
@@ -1,8 +1,10 @@
 package com.sleepkqq.sololeveling.gateway.config.grpc
 
 import com.sleepkqq.sololeveling.proto.config.DefaultGrpcClientConfig
+import com.sleepkqq.sololeveling.proto.config.LocaleClientInterceptor
 import com.sleepkqq.sololeveling.proto.player.PlayerServiceGrpc
 import com.sleepkqq.sololeveling.proto.user.UserServiceGrpc
+import io.grpc.ClientInterceptor
 import io.grpc.ManagedChannel
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
@@ -12,20 +14,24 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 @EnableConfigurationProperties(GrpcPlayerServiceProperties::class)
 class GrpcConfigClient(
-	private val properties: GrpcPlayerServiceProperties,
-	private val localeClientInterceptor: LocaleClientInterceptor
+	private val properties: GrpcPlayerServiceProperties
 ) : DefaultGrpcClientConfig() {
 
 	@Bean
 	fun playerManagedChannel(): ManagedChannel = createManagedChannel(properties)
 
 	@Bean
-	fun userServiceBlockingStub(): UserServiceGrpc.UserServiceBlockingStub =
+	fun localeClientInterceptor(): ClientInterceptor = LocaleClientInterceptor()
+
+	@Bean
+	fun userServiceBlockingStub(localeClientInterceptor: ClientInterceptor):
+			UserServiceGrpc.UserServiceBlockingStub =
 		UserServiceGrpc.newBlockingStub(playerManagedChannel())
 			.withInterceptors(localeClientInterceptor)
 
 	@Bean
-	fun playerServiceBlockingStub(): PlayerServiceGrpc.PlayerServiceBlockingStub =
+	fun playerServiceBlockingStub(localeClientInterceptor: ClientInterceptor):
+			PlayerServiceGrpc.PlayerServiceBlockingStub =
 		PlayerServiceGrpc.newBlockingStub(playerManagedChannel())
 			.withInterceptors(localeClientInterceptor)
 }
